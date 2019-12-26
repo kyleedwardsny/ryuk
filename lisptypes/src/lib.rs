f98e489db260843d082ec1587a765c876e02956c
Kyle Edwards <kyleedwardsny@gmail.com>
2019-12-25 19:05:25 -0500

    Refactor Value to generalize references
    lispparser has not yet been updated for this

--- lisptypes/src/lib.rs
+++ lisptypes/src/lib.rs
@@ -1,7 +1,5 @@
-use std::borrow::{Cow, ToOwned};
-use std::convert::TryFrom;
 use std::fmt::Formatter;
-use std::rc::Rc;
+use std::ops::Deref;
 
 #[derive(Debug)]
 pub struct Error {
@@ -36,44 +34,38 @@ impl std::fmt::Display for Error {
 
 pub type Result<T> = std::result::Result<T, Error>;
 
-// Workaround for a bug in rustc. See
-// https://github.com/rust-lang/rust/issues/47032.
 #[derive(Debug)]
-pub struct SizedHolder<T>(pub T);
+pub struct ValueSymbol<S: Deref<Target = str>>(pub S);
 
-impl<T: Clone> Clone for SizedHolder<T> {
-    fn clone(&self) -> SizedHolder<T> {
-        SizedHolder(self.0.clone())
-    }
-}
-
-#[derive(Debug, PartialEq)]
-pub struct ValueSymbol(pub Cow<'static, str>);
-
-impl Clone for ValueSymbol {
-    fn clone(&self) -> ValueSymbol {
-        ValueSymbol(self.0.clone())
+impl<S1, S2> PartialEq<ValueSymbol<S2>> for ValueSymbol<S1>
+where
+    S1: Deref<Target = str>,
+    S2: Deref<Target = str>,
+{
+    fn eq(&self, rhs: &ValueSymbol<S2>) -> bool {
+        *self.0 == *rhs.0
     }
 }
 
 #[derive(Debug)]
-pub struct ValueCons {
-    pub car: SizedHolder<ValueRef>,
-    pub cdr: SizedHolder<ValueRef>,
+pub struct ValueCons<V, S>
+where
+    V: Deref<Target = Value<V, S>>,
+    S: Deref<Target = str>,
+{
+    pub car: V,
+    pub cdr: V,
 }
 
-impl Clone for ValueCons {
-    fn clone(&self) -> ValueCons {
-        ValueCons {
-            car: self.car.clone(),
-            cdr: self.cdr.clone(),
-        }
-    }
-}
-
-impl PartialEq for ValueCons {
-    fn eq(&self, other: &Self) -> bool {
-        *self.car.0 == *other.car.0 && *self.cdr.0 == *other.cdr.0
+impl<V1, S1, V2, S2> PartialEq<ValueCons<V2, S2>> for ValueCons<V1, S1>
+where
+    V1: Deref<Target = Value<V1, S1>>,
+    S1: Deref<Target = str>,
+    V2: Deref<Target = Value<V2, S2>>,
+    S2: Deref<Target = str>,
+{
+    fn eq(&self, other: &ValueCons<V2, S2>) -> bool {
+        *self.car == *other.car && *self.cdr == *other.cdr
     }
 }
 
@@ -86,125 +78,88 @@ impl Clone for ValueBool {
     }
 }
 
-#[derive(Debug, PartialEq)]
-pub enum Value {
+#[derive(Debug)]
+pub enum Value<V, S>
+where
+    V: Deref<Target = Value<V, S>>,
+    S: Deref<Target = str>,
+{
     Nil,
-    Symbol(ValueSymbol),
-    Cons(ValueCons),
+    Symbol(ValueSymbol<S>),
+    Cons(ValueCons<V, S>),
     Bool(ValueBool),
 }
 
-impl ToOwned for Value {
-    type Owned = Rc<Value>;
-
-    fn to_owned(&self) -> Self::Owned {
-        Rc::new(match self {
-            Value::Nil => Value::Nil,
-            Value::Symbol(ValueSymbol(name)) => Value::Symbol(ValueSymbol(name.clone())),
-            Value::Cons(ValueCons { car, cdr }) => Value::Cons(ValueCons {
-                car: SizedHolder(car.0.clone()),
-                cdr: SizedHolder(cdr.0.clone()),
-            }),
-            Value::Bool(b) => Value::Bool(*b),
-        })
-    }
-}
-
-pub type ValueRef = Cow<'static, Value>;
-
-impl From<ValueSymbol> for ValueRef {
-    fn from(s: ValueSymbol) -> ValueRef {
-        ValueRef::Owned(Rc::new(Value::Symbol(s)))
-    }
-}
-
-impl From<ValueCons> for ValueRef {
-    fn from(c: ValueCons) -> ValueRef {
-        ValueRef::Owned(Rc::new(Value::Cons(c)))
-    }
-}
-
-impl From<ValueBool> for ValueRef {
-    fn from(b: ValueBool) -> ValueRef {
-        match b.0 {
-            true => ValueRef::Borrowed(&Value::Bool(ValueBool(true))),
-            false => ValueRef::Borrowed(&Value::Bool(ValueBool(false))),
+impl<V1, S1, V2, S2> PartialEq<Value<V2, S2>> for Value<V1, S1>
+where
+    V1: Deref<Target = Value<V1, S1>>,
+    S1: Deref<Target = str>,
+    V2: Deref<Target = Value<V2, S2>>,
+    S2: Deref<Target = str>,
+{
+    fn eq(&self, rhs: &Value<V2, S2>) -> bool {
+        match self {
+            Value::Nil => match rhs {
+                Value::Nil => true,
+                _ => false,
+            },
+            Value::Symbol(s1) => match rhs {
+                Value::Symbol(s2) => *s1 == *s2,
+                _ => false,
+            },
+            Value::Cons(c1) => match rhs {
+                Value::Cons(c2) => *c1 == *c2,
+                _ => false,
+            },
+            Value::Bool(b1) => match rhs {
+                Value::Bool(b2) => *b1 == *b2,
+                _ => false,
+            },
         }
     }
 }
 
-impl TryFrom<ValueRef> for ValueSymbol {
-    type Error = Error;
+pub type ValueStatic = Value<ValueStaticRef, &'static str>;
 
-    fn try_from(value: ValueRef) -> Result<Self> {
-        match &*value {
-            Value::Symbol(s) => Result::Ok(s.clone()),
-            _ => Result::Err(Error::new(
-                ErrorKind::IncorrectType,
-                "Incorrect type".to_string(),
-            )),
-        }
-    }
-}
+#[derive(Debug)]
+pub struct ValueStaticRef(pub &'static ValueStatic);
 
-impl TryFrom<ValueRef> for ValueCons {
-    type Error = Error;
+impl Deref for ValueStaticRef {
+    type Target = Value<ValueStaticRef, &'static str>;
 
-    fn try_from(value: ValueRef) -> Result<Self> {
-        match &*value {
-            Value::Cons(c) => Result::Ok(c.clone()),
-            _ => Result::Err(Error::new(
-                ErrorKind::IncorrectType,
-                "Incorrect type".to_string(),
-            )),
-        }
-    }
-}
-
-impl TryFrom<ValueRef> for ValueBool {
-    type Error = Error;
-
-    fn try_from(value: ValueRef) -> Result<Self> {
-        match &*value {
-            Value::Bool(b) => Result::Ok(b.clone()),
-            _ => Result::Err(Error::new(
-                ErrorKind::IncorrectType,
-                "Incorrect type".to_string(),
-            )),
-        }
+    fn deref(&self) -> &Self::Target {
+        self.0
     }
 }
 
 #[macro_export]
 macro_rules! nil {
     () => {
-        $crate::ValueRef::Borrowed(&$crate::Value::Nil)
+        $crate::ValueStaticRef(&$crate::Value::Nil)
     };
 }
 
 #[macro_export]
 macro_rules! sym {
-    ($name:literal) => {
-        $crate::ValueRef::Borrowed(&$crate::Value::Symbol($crate::ValueSymbol(
-            ::std::borrow::Cow::Borrowed($name),
-        )))
+    ($name:expr) => {
+        $crate::ValueStaticRef(&$crate::Value::Symbol($crate::ValueSymbol($name)))
     };
 }
 
 #[macro_export]
 macro_rules! cons {
     ($car:expr, $cdr:expr) => {
-        $crate::ValueRef::Owned(::std::rc::Rc::new($crate::Value::Cons($crate::ValueCons {
-            car: $crate::SizedHolder($car),
-            cdr: $crate::SizedHolder($cdr),
-        })))
+        $crate::ValueStaticRef(&$crate::Value::Cons($crate::ValueCons {
+            car: $car,
+            cdr: $cdr,
+        }))
     };
 }
 
 #[macro_export]
 macro_rules! bool {
-    ($b:literal) => {
-        $crate::ValueRef::Borrowed(&$crate::Value::Bool($crate::ValueBool($b)))
+    ($b:expr) => {
+        $crate::ValueStaticRef(&$crate::Value::Bool($crate::ValueBool($b)))
     };
 }
 
@@ -219,204 +174,114 @@ macro_rules! list {
 mod tests {
     #[test]
     fn test_nil_macro() {
-        let nil = nil!();
-        match nil {
-            std::borrow::Cow::Borrowed(v) => assert_eq!(*v, super::Value::Nil),
-            _ => panic!("Expected a borrowed Value"),
-        }
+        const NIL: super::ValueStaticRef = nil!();
+        assert_eq!(*NIL, super::ValueStatic::Nil);
     }
 
     #[test]
     fn test_sym_macro() {
-        let sym = sym!("sym");
-        match sym {
-            std::borrow::Cow::Borrowed(v) => match v {
-                super::Value::Symbol(s) => match s.0 {
-                    std::borrow::Cow::Borrowed(name) => assert_eq!(name, "sym"),
-                    _ => panic!("Expected a borrowed str"),
-                },
-                _ => panic!("Expected a Value::Symbol"),
-            },
-            _ => panic!("Expected a borrowed Value"),
+        const SYM: super::ValueStaticRef = sym!("sym");
+        match &*SYM {
+            super::Value::Symbol(s) => assert_eq!(s.0, "sym"),
+            _ => panic!("Expected a Value::Symbol"),
         }
     }
 
     #[test]
     fn test_cons_macro() {
-        let cons = cons!(sym!("sym"), nil!());
-        match cons {
-            std::borrow::Cow::Owned(v) => match &*v {
-                super::Value::Cons(c) => {
-                    match c.car {
-                        super::SizedHolder(std::borrow::Cow::Borrowed(car)) => match car {
-                            super::Value::Symbol(s) => assert_eq!(s.0, "sym"),
-                            _ => panic!("Expected a Value::Symbol"),
-                        },
-                        _ => panic!("Expected a borrowed Value"),
-                    }
-                    match c.cdr {
-                        super::SizedHolder(std::borrow::Cow::Borrowed(cdr)) => {
-                            assert_eq!(*cdr, super::Value::Nil)
-                        }
-                        _ => panic!("Expected a borrowed Value"),
-                    }
+        const CONS: super::ValueStaticRef = cons!(sym!("sym"), nil!());
+        match &*CONS {
+            super::Value::Cons(c) => {
+                match &*c.car {
+                    super::Value::Symbol(s) => assert_eq!(s.0, "sym"),
+                    _ => panic!("Expected a Value::Symbol"),
                 }
-                _ => panic!("Expected a Value::Cons"),
-            },
-            _ => panic!("Expected an owned Value"),
+                assert_eq!(*c.cdr, super::ValueStatic::Nil);
+            }
+            _ => panic!("Expected a Value::Cons"),
         }
     }
 
     #[test]
     fn test_bool_macro() {
-        let b = bool!(true);
-        match b {
-            std::borrow::Cow::Borrowed(v) => match &*v {
-                super::Value::Bool(b) => assert_eq!(b.0, true),
-                _ => panic!("Expected a Value::Bool"),
-            },
-            _ => panic!("Expected a borrowed Value"),
+        const B1: super::ValueStaticRef = bool!(true);
+        match &*B1 {
+            super::Value::Bool(b) => assert_eq!(b.0, true),
+            _ => panic!("Expected a Value::Bool"),
         }
-        let b = bool!(false);
-        match b {
-            std::borrow::Cow::Borrowed(v) => match &*v {
-                super::Value::Bool(b) => assert_eq!(b.0, false),
-                _ => panic!("Expected a Value::Bool"),
-            },
-            _ => panic!("Expected a borrowed Value"),
+        const B2: super::ValueStaticRef = bool!(false);
+        match &*B2 {
+            super::Value::Bool(b) => assert_eq!(b.0, false),
+            _ => panic!("Expected a Value::Bool"),
         }
     }
 
     #[test]
     fn test_list_macro() {
-        let l = list!();
-        match l {
-            std::borrow::Cow::Borrowed(v) => assert_eq!(*v, super::Value::Nil),
-            _ => panic!("Expected a borrowed Value"),
+        const LIST1: super::ValueStaticRef = list!();
+        assert_eq!(*LIST1, super::ValueStatic::Nil);
+
+        const LIST2: super::ValueStaticRef = list!(sym!("sym1"));
+        match &*LIST2 {
+            super::Value::Cons(c) => {
+                match &*c.car {
+                    super::Value::Symbol(s) => assert_eq!(s.0, "sym1"),
+                    _ => panic!("Expected a Value::Symbol"),
+                }
+                assert_eq!(*c.cdr, super::ValueStatic::Nil);
+            }
+            _ => panic!("Expected a Value::Cons"),
         }
 
-        let l = list!(sym!("sym1"));
-        match l {
-            std::borrow::Cow::Owned(v) => match &*v {
-                super::Value::Cons(c) => {
-                    match c.car {
-                        super::SizedHolder(std::borrow::Cow::Borrowed(car)) => match car {
-                            super::Value::Symbol(s) => assert_eq!(s.0, "sym1"),
+        const LIST3: super::ValueStaticRef = list!(sym!("sym1"), sym!("sym2"));
+        match &*LIST3 {
+            super::Value::Cons(c) => {
+                match &*c.car {
+                    super::Value::Symbol(s) => assert_eq!(s.0, "sym1"),
+                    _ => panic!("Expected a Value::Symbol"),
+                }
+                match &*c.cdr {
+                    super::Value::Cons(c) => {
+                        match &*c.car {
+                            super::Value::Symbol(s) => assert_eq!(s.0, "sym2"),
                             _ => panic!("Expected a Value::Symbol"),
-                        },
-                        _ => panic!("Expected a borrowed Value"),
-                    }
-                    match c.cdr {
-                        super::SizedHolder(std::borrow::Cow::Borrowed(cdr)) => {
-                            assert_eq!(*cdr, super::Value::Nil)
                         }
-                        _ => panic!("Expected a borrowed Value"),
+                        assert_eq!(*c.cdr, super::ValueStatic::Nil);
                     }
+                    _ => panic!("Expected a Value::Cons"),
                 }
-                _ => panic!("Expected a Value::Cons"),
-            },
-            _ => panic!("Expected a borrowed Value"),
+            }
+            _ => panic!("Expected a Value::Cons"),
         }
 
-        let l = list!(sym!("sym1"), sym!("sym2"));
-        match l {
-            std::borrow::Cow::Owned(v) => match &*v {
-                super::Value::Cons(c) => {
-                    match c.car {
-                        super::SizedHolder(std::borrow::Cow::Borrowed(car)) => match &*car {
-                            super::Value::Symbol(s) => assert_eq!(s.0, "sym1"),
+        const LIST4: super::ValueStaticRef = list!(sym!("sym1"), sym!("sym2"), sym!("sym3"));
+        match &*LIST4 {
+            super::Value::Cons(c) => {
+                match &*c.car {
+                    super::Value::Symbol(s) => assert_eq!(s.0, "sym1"),
+                    _ => panic!("Expected a Value::Symbol"),
+                }
+                match &*c.cdr {
+                    super::Value::Cons(c) => {
+                        match &*c.car {
+                            super::Value::Symbol(s) => assert_eq!(s.0, "sym2"),
                             _ => panic!("Expected a Value::Symbol"),
-                        },
-                        _ => panic!("Expected a borrowed Value"),
-                    }
-                    match &c.cdr {
-                        super::SizedHolder(std::borrow::Cow::Owned(cdr)) => match &**cdr {
+                        }
+                        match &*c.cdr {
                             super::Value::Cons(c) => {
-                                match c.car {
-                                    super::SizedHolder(std::borrow::Cow::Borrowed(car)) => {
-                                        match &*car {
-                                            super::Value::Symbol(s) => assert_eq!(s.0, "sym2"),
-                                            _ => panic!("Expected a Value::Symbol"),
-                                        }
-                                    }
-                                    _ => panic!("Expected a borrowed Value"),
+                                match &*c.car {
+                                    super::Value::Symbol(s) => assert_eq!(s.0, "sym3"),
+                                    _ => panic!("Expected a Value::Symbol"),
                                 }
-                                match c.cdr {
-                                    super::SizedHolder(std::borrow::Cow::Borrowed(cdr)) => {
-                                        assert_eq!(*cdr, super::Value::Nil)
-                                    }
-                                    _ => panic!("Expected a borrowed Value"),
-                                }
+                                assert_eq!(*c.cdr, super::ValueStatic::Nil);
                             }
                             _ => panic!("Expected a Value::Cons"),
-                        },
-                        _ => panic!("Expected an owned Value"),
+                        }
                     }
+                    _ => panic!("Expected a Value::Cons"),
                 }
-                _ => panic!("Expected a Value::Cons"),
-            },
-            _ => panic!("Expected a borrowed Value"),
-        }
-
-        let l = list!(sym!("sym1"), sym!("sym2"), sym!("sym3"));
-        match l {
-            std::borrow::Cow::Owned(v) => match &*v {
-                super::Value::Cons(c) => {
-                    match c.car {
-                        super::SizedHolder(std::borrow::Cow::Borrowed(car)) => match &*car {
-                            super::Value::Symbol(s) => assert_eq!(s.0, "sym1"),
-                            _ => panic!("Expected a Value::Symbol"),
-                        },
-                        _ => panic!("Expected a borrowed Value"),
-                    }
-                    match &c.cdr {
-                        super::SizedHolder(std::borrow::Cow::Owned(cdr)) => match &**cdr {
-                            super::Value::Cons(c) => {
-                                match c.car {
-                                    super::SizedHolder(std::borrow::Cow::Borrowed(car)) => {
-                                        match &*car {
-                                            super::Value::Symbol(s) => assert_eq!(s.0, "sym2"),
-                                            _ => panic!("Expected a Value::Symbol"),
-                                        }
-                                    }
-                                    _ => panic!("Expected a borrowed Value"),
-                                }
-                                match &c.cdr {
-                                    super::SizedHolder(std::borrow::Cow::Owned(cdr)) => {
-                                        match &**cdr {
-                                            super::Value::Cons(c) => {
-                                                match c.car {
-                                                    super::SizedHolder(
-                                                        std::borrow::Cow::Borrowed(car),
-                                                    ) => match &*car {
-                                                        super::Value::Symbol(s) => {
-                                                            assert_eq!(s.0, "sym3")
-                                                        }
-                                                        _ => panic!("Expected a Value::Symbol"),
-                                                    },
-                                                    _ => panic!("Expected a borrowed Value"),
-                                                }
-                                                match c.cdr {
-                                                    super::SizedHolder(
-                                                        std::borrow::Cow::Borrowed(cdr),
-                                                    ) => assert_eq!(*cdr, super::Value::Nil),
-                                                    _ => panic!("Expected a borrowed Value"),
-                                                }
-                                            }
-                                            _ => panic!("Expected a Value::Cons"),
-                                        }
-                                    }
-                                    _ => panic!("Expected an owned Value"),
-                                }
-                            }
-                            _ => panic!("Expected a Value::Cons"),
-                        },
-                        _ => panic!("Expected an owned Value"),
-                    }
-                }
-                _ => panic!("Expected a Value::Cons"),
-            },
-            _ => panic!("Expected a borrowed Value"),
+            }
+            _ => panic!("Expected a Value::Cons"),
         }
     }
 }
