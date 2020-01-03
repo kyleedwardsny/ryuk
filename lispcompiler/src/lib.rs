c4b115060d793d981346a5344b97b083b1c450d0
Kyle Edwards <kyleedwardsny@gmail.com>
2020-01-02 22:48:09 -0500

    Remove ValueTypeNonNil

--- lispcompiler/src/lib.rs
+++ lispcompiler/src/lib.rs
@@ -12,17 +12,12 @@ pub enum ValueType {
 #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub struct ValueTypeList {
     pub items: BTreeSet<ValueType>,
-    pub tail: BTreeSet<ValueTypeNonNil>,
+    pub tail: BTreeSet<ValueTypeNonList>,
 }
 
 #[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
 pub enum ValueTypeNonList {
     Nil,
-    NonNil(ValueTypeNonNil),
-}
-
-#[derive(Debug, Eq, Ord, PartialEq, PartialOrd)]
-pub enum ValueTypeNonNil {
     UnqualifiedSymbol,
     QualifiedSymbol,
     Bool,
@@ -43,13 +38,18 @@ where
             Value::Cons(c) => {
                 let mut l = ValueTypeList {
                     items: BTreeSet::from_iter(vec![ValueType::from(c.car.borrow())]),
-                    tail: BTreeSet::new(),
+                    tail: BTreeSet::from_iter(vec![ValueTypeNonList::Nil]),
                 };
                 for item in LispList::<T>::new(c.cdr.clone()) {
                     match item {
-                        LispListItem::Item(i) => l.items.insert(ValueType::from(i.borrow())),
-                        LispListItem::Tail(t) => l.tail.insert(ValueTypeNonNil::from(t.borrow())),
-                    };
+                        LispListItem::Item(i) => {
+                            l.items.insert(ValueType::from(i.borrow()));
+                        }
+                        LispListItem::Tail(t) => {
+                            l.tail.clear();
+                            l.tail.insert(ValueTypeNonList::from(t.borrow()));
+                        }
+                    }
                 }
                 ValueType::List(l)
             }
@@ -67,28 +67,14 @@ where
     fn from(v: &Value<T>) -> Self {
         match v {
             Value::Nil => ValueTypeNonList::Nil,
-            _ => ValueTypeNonList::NonNil(ValueTypeNonNil::from(v)),
-        }
-    }
-}
-
-impl<T> From<&Value<T>> for ValueTypeNonNil
-where
-    T: ValueTypes + ?Sized,
-    for<'a> &'a <T::SemverTypes as SemverTypes>::Semver: IntoIterator<Item = &'a u64>,
-    T::ValueRef: Clone,
-{
-    fn from(v: &Value<T>) -> Self {
-        match v {
-            Value::Nil => panic!("Unexpected Value::Nil"),
-            Value::UnqualifiedSymbol(_) => ValueTypeNonNil::UnqualifiedSymbol,
-            Value::QualifiedSymbol(_) => ValueTypeNonNil::QualifiedSymbol,
+            Value::UnqualifiedSymbol(_) => ValueTypeNonList::UnqualifiedSymbol,
+            Value::QualifiedSymbol(_) => ValueTypeNonList::QualifiedSymbol,
             Value::Cons(_) => panic!("Unexpected Value::Cons"),
-            Value::Bool(_) => ValueTypeNonNil::Bool,
-            Value::String(_) => ValueTypeNonNil::String,
-            Value::Semver(_) => ValueTypeNonNil::Semver,
-            Value::LanguageDirective(_) => ValueTypeNonNil::LanguageDirective,
-            Value::Procedure(_) => ValueTypeNonNil::Procedure,
+            Value::Bool(_) => ValueTypeNonList::Bool,
+            Value::String(_) => ValueTypeNonList::String,
+            Value::Semver(_) => ValueTypeNonList::Semver,
+            Value::LanguageDirective(_) => ValueTypeNonList::LanguageDirective,
+            Value::Procedure(_) => ValueTypeNonList::Procedure,
         }
     }
 }
@@ -105,34 +91,29 @@ mod tests {
     }
 
     #[test]
-    fn test_from_non_nil() {
-        assert_eq!(
-            ValueTypeNonNil::from(uqsym!("uqsym")),
-            ValueTypeNonNil::UnqualifiedSymbol
-        );
-        assert_eq!(
-            ValueTypeNonNil::from(qsym!("p", "qsym")),
-            ValueTypeNonNil::QualifiedSymbol
-        );
-        assert_eq!(ValueTypeNonNil::from(bool!(true)), ValueTypeNonNil::Bool);
-        assert_eq!(ValueTypeNonNil::from(str!("str")), ValueTypeNonNil::String);
-        assert_eq!(ValueTypeNonNil::from(v![1, 0]), ValueTypeNonNil::Semver);
-        assert_eq!(
-            ValueTypeNonNil::from(lang_kira![1, 0]),
-            ValueTypeNonNil::LanguageDirective
-        );
-        assert_eq!(
-            ValueTypeNonNil::from(proc!(1, &static_f1)),
-            ValueTypeNonNil::Procedure
-        );
-    }
-
-    #[test]
     fn test_from_non_list() {
         assert_eq!(ValueTypeNonList::from(nil!()), ValueTypeNonList::Nil);
         assert_eq!(
-            ValueTypeNonList::from(bool!(true)),
-            ValueTypeNonList::NonNil(ValueTypeNonNil::Bool)
+            ValueTypeNonList::from(uqsym!("uqsym")),
+            ValueTypeNonList::UnqualifiedSymbol
+        );
+        assert_eq!(
+            ValueTypeNonList::from(qsym!("p", "qsym")),
+            ValueTypeNonList::QualifiedSymbol
+        );
+        assert_eq!(ValueTypeNonList::from(bool!(true)), ValueTypeNonList::Bool);
+        assert_eq!(
+            ValueTypeNonList::from(str!("str")),
+            ValueTypeNonList::String
+        );
+        assert_eq!(ValueTypeNonList::from(v![1, 0]), ValueTypeNonList::Semver);
+        assert_eq!(
+            ValueTypeNonList::from(lang_kira![1, 0]),
+            ValueTypeNonList::LanguageDirective
+        );
+        assert_eq!(
+            ValueTypeNonList::from(proc!(1, &static_f1)),
+            ValueTypeNonList::Procedure
         );
     }
 
@@ -148,19 +129,17 @@ mod tests {
             ValueType::List(ValueTypeList {
                 items: BTreeSet::from_iter(vec![
                     ValueType::NonList(ValueTypeNonList::Nil),
-                    ValueType::NonList(ValueTypeNonList::NonNil(ValueTypeNonNil::Bool)),
-                    ValueType::NonList(ValueTypeNonList::NonNil(ValueTypeNonNil::String)),
+                    ValueType::NonList(ValueTypeNonList::Bool),
+                    ValueType::NonList(ValueTypeNonList::String),
                     ValueType::List(ValueTypeList {
                         items: BTreeSet::from_iter(vec![
-                            ValueType::NonList(ValueTypeNonList::NonNil(
-                                ValueTypeNonNil::UnqualifiedSymbol
-                            )),
-                            ValueType::NonList(ValueTypeNonList::NonNil(ValueTypeNonNil::Bool)),
+                            ValueType::NonList(ValueTypeNonList::UnqualifiedSymbol),
+                            ValueType::NonList(ValueTypeNonList::Bool),
                         ]),
-                        tail: BTreeSet::from_iter(vec![ValueTypeNonNil::QualifiedSymbol,]),
+                        tail: BTreeSet::from_iter(vec![ValueTypeNonList::QualifiedSymbol]),
                     }),
                 ]),
-                tail: BTreeSet::new(),
+                tail: BTreeSet::from_iter(vec![ValueTypeNonList::Nil]),
             })
         );
     }
